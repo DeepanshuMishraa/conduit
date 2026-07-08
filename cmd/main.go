942883af2f0a58fd618b1b3511e0a53405b50071
Deepanshu Mishra <d4deepanshu723@gmail.com>
2026-07-08 12:21:26 +0530

    hook gin and configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,29 @@
-package main;
+package main
 
+import (
+	"log"
 
-func main(){
+	"github.com/DeepanshuMishraa/conduit.git/config"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	cfg, err := config.Load()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := gin.Default()
+	router.SetTrustedProxies(nil)
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	log.Println("[API] RUNNING ON PORT", cfg.PORT)
+
+	router.Run(":" + cfg.PORT)
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DATABASE_URL string
+	PORT         string
+}
+
+func Load() (*Config, error) {
+	err := godotenv.Load()
+
+	if err != nil {
+		return nil, err
+	}
+
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+	PORT := os.Getenv("PORT")
+
+	if DATABASE_URL == "" || PORT == "" {
+		return nil, errors.New("Environment variables not set")
+	}
+
+	log.Println("All env vars loaded")
+
+	return &Config{
+		DATABASE_URL: DATABASE_URL,
+		PORT:         PORT,
+	}, nil
+}
